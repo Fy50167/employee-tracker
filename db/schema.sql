aa2ce548d5efdf9cd66f123f023a6a6959eb3268
Francis Yang <fy50167@gmail.com>
2023-09-08 18:29:47 -0700

    Making schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -4,32 +4,24 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE departments (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    id INT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    department VARCHAR(30) NOT NULL,
-    salary INT NOT NULL
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    department VARCHAR(30) NOT NULL,
-    salary INT NOT NULL,
-    manager VARCHAR(30)
-    FOREIGN KEY (manager)
-    REFERENCES employees(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (title)
-    REFERENCES roles(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (department)
-    REFERENCES departments(id)
-    ON DELETE SET NULL,
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
